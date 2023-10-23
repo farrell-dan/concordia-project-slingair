aecfd6445f1fa7b6b5fee937d530c3ac89eebc19
Dear Lee Ray Gifwandi <ray@dears-mbp.home>
2023-10-23 19:12:39 -0400

    updateReservation

--- backend/handlers/updateReservation.js
+++ backend/handlers/updateReservation.js
@@ -1,6 +1,49 @@
 "use strict";
 
 // updates a specified reservation
-const updateReservation = (req, res) => {};
+
+const { MongoClient } = require("mongodb");
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const updateReservation = async (req, res) => {
+    const {    
+        _id,
+        flight,
+        seat,
+        givenName,
+        surname,
+        email} = req.body
+    const client = new MongoClient(MONGO_URI, options);
+
+    console.log("hello")
+    console.log(req.body._id)
+  
+    try {
+      await client.connect();
+      const db = client.db("Slingair");
+      const oldReservation = await db.collection("reservations").findOne({ _id:  req.body._id});
+      console.log(oldReservation)
+
+      if(!oldReservation) {
+        return res.status(404).json({ error: "Reservation not found :(" });
+      }
+
+      await db.collection("flights").updateOne({ flight: oldReservation.flight, "seats.id": oldReservation.seat }, {$set: {"seats.$.isAvailable": true}});
+      await db.collection("flights").updateOne({ flight: req.body.flight, "seats.id": req.body.seat }, {$set: {"seats.$.isAvailable": false}});
+
+      const newReservation = await db.collection("reservations").updateOne({ _id:  req.body._id},{$set: req.body});
+      
+      return res.status(200).json({status: 200, data: newReservation});
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Server Error" });
+    } finally {
+      client.close();
+    }
+};
 
 module.exports = updateReservation;
